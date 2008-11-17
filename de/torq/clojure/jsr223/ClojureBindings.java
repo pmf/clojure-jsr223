a2368131760ed3329124e77b31cb423030d2387d
Philipp Frank <pmf@debian.localdomain>
2008-11-17 22:52:11 +0100

    Some infrastructure for using ExectutorService.

--- de/torq/clojure/jsr223/ClojureBindings.java
+++ de/torq/clojure/jsr223/ClojureBindings.java
@@ -3,8 +3,11 @@ package de.torq.clojure.jsr223;
 import javax.script.Bindings;
 import java.util.HashMap;
 
-// TODO: use APersistentMap, since it implements Associative and therefore can
-// be used to push and pop Clojure-bindings
+// TODO:
+// - use PersistentHashMap, since it implements Associative and therefore can
+//   be used to push and pop Clojure-bindings
+// - support fully qualified names (as desribed in JSR 223 released version, p. 141)
+//
 public class ClojureBindings extends HashMap<String, Object>
                              implements Bindings
 {
--- de/torq/clojure/jsr223/ClojureScriptEngine.java
+++ de/torq/clojure/jsr223/ClojureScriptEngine.java
@@ -2,21 +2,125 @@ package de.torq.clojure.jsr223;
 
 import java.io.Reader;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
 import javax.script.Bindings;
 import javax.script.AbstractScriptEngine;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptContext;
 import javax.script.Invocable;
 
+import clojure.lang.Var;
+
+/**
+ * The design of Clojure is somewhat special in that there is no way to get
+ * some kind of context-object that serves as handle for a certain instance of
+ * an engine; instead, clojure.lang.RT works by using thread-locals as handle.
+ * This has the implication that we must manage internal handles to different
+ * engines on a separate thread for each instance. In other words, the instance
+ * of ClojureScriptEngine (which wraps RT) is a regular object, but internally
+ * it has to communicate with a reference to RT on another thread. This is not
+ * important to users of this class, I just felt a need to justify my rather
+ * strange design.
+ *
+ * TODO: use java.util.concurrent.Executors.newSingleThreadExecutor
+ */
 public class ClojureScriptEngine extends AbstractScriptEngine
                                  implements Invocable
 {
+
+    private static final ScriptEngineFactory factory = new ClojureScriptEngineFactory();
+
+    private AtomicBoolean closed = new AtomicBoolean(false);
+    private Bindings engineBindings = null;
+    private final ExecutorService executor;
+
     public ClojureScriptEngine()
     {
+        // TODO: use ScriptContext's engineScope/globalScope; store bindings
+        // locally; push on stack before each invocation and pop off stack
+        // after each invocation of a method
         //Var.pushThreadBindings();
         // How to call popThreadBindings?
+        executor = Executors.newSingleThreadExecutor();
     }
 
+    /**
+     * Returns true if this ScriptEngine has been closed; further invocation of
+     * any methods will have undefined behaviour.
+     */
+    public boolean isClosed()
+    {
+        return closed.get();
+    }
+
+    /*
+     * Closes this ScriptEngine.
+     * TODO: needed because I need to call Var.popThreadBindings when we are
+     *       done with this instance; someone please find a better way...
+     */
+    public void close()
+    {
+        closed.compareAndSet(false, true);
+
+        // Var.popThreadBindings();
+    }
+
+    @Override
+    public ScriptEngineFactory getFactory()
+    {
+        return factory;
+    }
+
+    @Override
+    public Bindings createBindings()
+    {
+        return new ClojureBindings();
+    }
+
+    @Override
+    public Object eval(Reader reader, ScriptContext context)
+    {
+        return null;
+    }
+
+    @Override
+    public Object eval(String script, ScriptContext context)
+    {
+        return null;
+    }
+
+    @Override // required by Invocable-interface
+    public <T> T getInterface(Class<T> clasz)
+    {
+        return null;
+    }
+
+    @Override // required by Invocable-interface
+    public <T> T getInterface(Object thiz, Class<T> clasz)
+    {
+        return null;
+    }
+
+    @Override // required by Invocable-interface
+    public Object invokeFunction(String name, Object... args)
+    {
+        return null;
+    }
+
+    @Override // required by Invocable-interface
+    public Object invokeMethod(Object thiz, String name, Object... args)
+    {
+        return null;
+    }
+}
+
+class InternalEngine extends AbstractScriptEngine
+                             implements Invocable
+{
     @Override
     public ScriptEngineFactory getFactory()
     {
@@ -61,6 +165,49 @@ public class ClojureScriptEngine extends AbstractScriptEngine
 
     @Override // required by Invocable-interface
     public Object invokeMethod(Object thiz, String name, Object... args)
+    {
+        return null;
+    }
+}
+
+class CallableClojureInitialization implements Callable<Object>
+{
+    public CallableClojureInitialization()
+    {
+    }
+
+    public Object call()
+    {
+        Var.pushThreadBindings(null);
+        return null;
+    }
+}
+
+class CallableClojureFinalization implements Callable<Object>
+{
+    public CallableClojureFinalization()
+    {
+    }
+
+    public Object call()
+    {
+        Var.popThreadBindings();
+        return null;
+    }
+}
+
+class CallableEval implements Callable<Object>
+{
+    private final Reader reader;
+    private final ScriptContext context;
+
+    public CallableEval(Reader reader, ScriptContext context)
+    {
+        this.reader = reader;
+        this.context = context;
+    }
+
+    public Object call()
     {
         return null;
     }
