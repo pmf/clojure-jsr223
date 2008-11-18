70a10d8d0155238f3dabbc7ac48399e36d1f4bdf
Philipp Frank <pmf@debian.localdomain>
2008-11-18 15:35:31 +0100

    First success: simple evaluation works.

--- de/torq/clojure/jsr223/ClojureBindings.java
+++ de/torq/clojure/jsr223/ClojureBindings.java
@@ -2,14 +2,46 @@ package de.torq.clojure.jsr223;
 
 import javax.script.Bindings;
 import java.util.HashMap;
+import java.util.Map;
+
+import clojure.lang.Associative;
+import clojure.lang.PersistentHashMap;
+import clojure.lang.RT;
+import clojure.lang.Var;
 
 // TODO:
-// - use PersistentHashMap, since it implements Associative and therefore can
-//   be used to push and pop Clojure-bindings
 // - support fully qualified names (as desribed in JSR 223 released version, p. 141)
-//
 public class ClojureBindings extends HashMap<String, Object>
                              implements Bindings
 {
+
+    public static final String nsUser = "user";
+    public static final String nsClojure = "clojure.core";
+
+    public ClojureBindings()
+    {
+        super();
+    }
+
+    public ClojureBindings(Associative a)
+    {
+        super();
+    }
+
+    /* Convert an arbitrary Bindings-implementation to an Associative-instance
+     * (since JSR223 demands that we are able to accept any
+     * Bindings-implementation.
+     */
+    public static Associative toAssociative(Bindings b)
+    {
+        Associative result = PersistentHashMap.create();
+        for (Map.Entry<String, Object> e : b.entrySet())
+        {
+            Var var = RT.var(nsUser, e.getKey());
+            result = result.assoc(var, var.get());
+        }
+
+        return result;
+    }
 }
 
--- de/torq/clojure/jsr223/ClojureScriptEngine.java
+++ de/torq/clojure/jsr223/ClojureScriptEngine.java
@@ -1,22 +1,32 @@
 package de.torq.clojure.jsr223;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringReader;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import javax.script.Bindings;
 import javax.script.AbstractScriptEngine;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptContext;
+import javax.script.Bindings;
 import javax.script.Invocable;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngineFactory;
 import javax.script.ScriptException;
 
+import clojure.lang.Associative;
+import clojure.lang.Compiler;
+import clojure.lang.LineNumberingPushbackReader;
+import clojure.lang.LispReader;
+import clojure.lang.RT;
+import clojure.lang.Symbol;
 import clojure.lang.Var;
 
 /**
@@ -29,8 +39,6 @@ import clojure.lang.Var;
  * it has to communicate with a reference to RT on another thread. This is not
  * important to users of this class, I just felt a need to justify my rather
  * strange design.
- *
- * TODO: use java.util.concurrent.Executors.newSingleThreadExecutor
  */
 public class ClojureScriptEngine extends AbstractScriptEngine
                                  implements Invocable
@@ -39,8 +47,31 @@ public class ClojureScriptEngine extends AbstractScriptEngine
     private static final ScriptEngineFactory factory = new ClojureScriptEngineFactory();
 
     private AtomicBoolean closed = new AtomicBoolean(false);
-    private Bindings engineBindings = null;
     private final ExecutorService executor;
+
+    // BEGIN From clojure.lang.Repl
+    static final Symbol USER = Symbol.create(ClojureBindings.nsUser);
+    static final Symbol CLOJURE = Symbol.create(ClojureBindings.nsClojure);
+
+    static final Var in_ns = RT.var(ClojureBindings.nsClojure, "in-ns");
+    static final Var refer = RT.var(ClojureBindings.nsClojure, "refer");
+    static final Var ns = RT.var(ClojureBindings.nsClojure, "*ns*");
+    static final Var compile_path = RT.var(ClojureBindings.nsClojure, "*compile-path*");
+    static final Var warn_on_reflection = RT.var(ClojureBindings.nsClojure, "*warn-on-reflection*");
+    static final Var print_meta = RT.var(ClojureBindings.nsClojure, "*print-meta*");
+    static final Var print_length = RT.var(ClojureBindings.nsClojure, "*print-length*");
+    static final Var print_level = RT.var(ClojureBindings.nsClojure, "*print-level*");
+    // END From clojure.lang.Repl
+
+    private ClojureBindings engineBindings = new ClojureBindings();
+    private static final Associative globalBindings = RT.map(
+        ns, ns.get(),
+        warn_on_reflection, warn_on_reflection.get(),
+        print_meta, print_meta.get(),
+        print_length, print_length.get(),
+        print_level, print_level.get(),
+        compile_path, "classes"
+    );
 
     public ClojureScriptEngine()
     {
@@ -48,8 +79,8 @@ public class ClojureScriptEngine extends AbstractScriptEngine
         // locally; push on stack before each invocation and pop off stack
         // after each invocation of a method
         //Var.pushThreadBindings();
-        // How to call popThreadBindings?
         executor = Executors.newSingleThreadExecutor();
+        submitAndGetResult(new CallableClojureInitialization(globalBindings));
     }
 
     /**
@@ -72,6 +103,21 @@ public class ClojureScriptEngine extends AbstractScriptEngine
         {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * We need to perform some cleanup (Clojure-related stuff has to be done on
+     * the executor's thread).
+     */
+    protected void finalize() throws Throwable
+    {
+        System.out.println("entering: finalize()");
+        submitAndGetResult(new CallableClojureFinalization());
+        // TODO: we need to make sure the thread is interruptible;
+        // newSingleThreadExecutor does not make any guarantees about that, so
+        // we need to find another mechanism for spawning the thread.
+        executor.shutdownNow();
+        System.out.println("leaving: finalize()");
     }
 
     /**
@@ -118,7 +164,10 @@ public class ClojureScriptEngine extends AbstractScriptEngine
     @Override
     public Object eval(String script, ScriptContext context)
     {
-        return eval(new StringReader(script), context);
+        System.out.println("entering: eval(String script, ScriptContext context)");
+        Object result = eval(new StringReader(script), context);
+        System.out.println("leaving: eval(String script, ScriptContext context)");
+        return result;
     }
 
     @Override // required by Invocable-interface
@@ -200,15 +249,29 @@ class InternalEngine extends AbstractScriptEngine
 
 class CallableClojureInitialization implements Callable<Object>
 {
-    public CallableClojureInitialization()
+    private final Associative bindings;
+
+    public CallableClojureInitialization(Associative bindings)
     {
+        this.bindings = bindings;
     }
 
     public Object call()
     {
-        Var.pushThreadBindings(null);
-        return null;
+        try
+        {
+            Var.pushThreadBindings(bindings);
+            ClojureScriptEngine.in_ns.invoke(ClojureScriptEngine.USER);
+            ClojureScriptEngine.refer.invoke(ClojureScriptEngine.CLOJURE);
+
+            return null;
+        }
+        catch (Exception e )
+        {
+            throw new RuntimeException(e);
+        }
     }
+
 }
 
 class CallableClojureFinalization implements Callable<Object>
@@ -237,7 +300,59 @@ class CallableEval implements Callable<Object>
 
     public Object call()
     {
-        return null;
+        Associative a = ClojureBindings.toAssociative(context.getBindings(ScriptContext.ENGINE_SCOPE));
+        return handleInput();
+    }
+
+    private Object handleInput()
+    {
+        Object result = null;
+        try
+        {
+            //repl IO support
+            LineNumberingPushbackReader rdr = new LineNumberingPushbackReader(reader);
+            OutputStreamWriter w = (OutputStreamWriter) RT.OUT.get();//new OutputStreamWriter(System.out);
+            Object EOF = new Object();
+
+            //start the loop
+            w.write("Clojure\n");
+            for(; ;)
+            {
+                try
+                {
+                    //w.write(Compiler.currentNS().name + "=> ");
+                    w.write("=> ");
+                    w.flush();
+                    Object r = LispReader.read(rdr, false, EOF, false);
+                    if(r == EOF)
+                    {
+                        w.write("\n");
+                        w.flush();
+                        break;
+                    }
+                    Object ret = Compiler.eval(r);
+                    result = ret;
+                    RT.print(ret, w);
+                    w.write('\n');
+                    //w.flush();
+                }
+                catch(Throwable e)
+                {
+                    Throwable c = e;
+                    while(c.getCause() != null)
+                    {
+                        c = c.getCause();
+                    }
+                    ((PrintWriter) RT.ERR.get()).println(e instanceof Compiler.CompilerException ? e : c);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 }
 
