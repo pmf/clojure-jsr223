9019164e96f0cd42c0aa8d1cb09fda0d94aa977b
Philipp Frank <phil.frank@torq.de>
2008-11-25 05:09:18 +0100

    Missing initial value for bindings mapped to an Associative.

--- de/torq/clojure/jsr223/ClojureBindings.java
+++ de/torq/clojure/jsr223/ClojureBindings.java
@@ -29,16 +29,24 @@ public class ClojureBindings extends HashMap<String, Object>
     }
 
     /* Convert an arbitrary Bindings-implementation to an Associative-instance
-     * (since JSR223 demands that we are able to accept any
-     * Bindings-implementation.
+     * (since JSR223 demands that we are able to accept any * Bindings-implementation.
+     *
+     * NOTE: Due to thread-local vars, this must be called on the correct thread!
      */
     public static Associative toAssociative(Bindings b)
     {
         Associative result = PersistentHashMap.create();
         for (Map.Entry<String, Object> e : b.entrySet())
         {
-            Var var = RT.var(nsUser, e.getKey());
-            result = result.assoc(var, var.get());
+            // This will create a root binding if one does not exist. Purely
+            // thread-local definitions are not possible in Clojure (or Java).
+            // TODO: We could let the root binding unbound (excuse the confuse
+            // terminology) by passing null to RT.var. This would prevent sharing
+            // between ClojureScriptEngine-instances by throwing an
+            // IllegalStateException when trying to get the value of the var,
+            // but that's a matter of taste, I think.
+            Var var = RT.var(nsUser, e.getKey(), e.getValue());
+            result = result.assoc(var, e.getValue());
         }
 
         return result;
