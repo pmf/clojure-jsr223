3c45354dc865692b21cb537ac283a0ae8290e3ed
Philipp Frank <phil.frank@torq.de>
2008-11-25 07:09:00 +0100

    Changed assertion messages.

--- test/test.clj
+++ test/test.clj
@@ -14,21 +14,21 @@
 (def cse (.getEngineByName sem "Clojure"))
 
 (.put cse "foo" "bar")
-(my-assert "put() did not set the root-bound var to the correct value"
+(my-assert "put() must set the root-bound var to the correct value"
         (= "bar" (.eval cse "foo")))
 
 (def cse2 (.getEngineByName sem "Clojure"))
 
-(my-assert "evaluating the var did not return the correct result"
+(my-assert "evaluating the var must return the correct result"
         (= "bar" (.eval cse2 "foo")))
 
 (.put cse2 "foo" "bazz")
-(my-assert "put() did not set the thread-local var to the correct value"
+(my-assert "put() must set the thread-local var to the correct value"
         (= "bazz" (.eval cse2 "foo")))
 
-(my-assert "get() did not return the correct value for the var (expected, since get() is not implemented yet)"
+(my-assert "get() must return the correct value for the var (expected, since get() is not implemented yet)"
         (= "bazz" (.get cse2 "foo2")))
 
 ;; Make sure the changes in cse2 do not affect the binding of the var foo in cse
-(my-assert "evaluating the var die not return the correct value"
+(my-assert "evaluating the var must return the correct value"
         (= "bar" (.eval cse "foo")))
